3bc131123020f45b795e6569d3ecba8ba87073ad
sa1amander83 <virom8@yandex.ru>
2020-12-10 00:15:40 +0300

    добавлено меню входа

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,53 @@
 function  pageLoaded() {
-const menuBtn = document.querySelector(".menu__button");
-const closeMenuButton= document.querySelector(".menu__mobile-close");
-const menuMobile=document.querySelector(".menu__mobile");
-const ulwrap=document.querySelector(".ulwrap");
+    const menuBtn = document.querySelector(".menu__button");
+    const closeMenuButton = document.querySelector(".menu__mobile-close");
+    const menuMobile = document.querySelector(".menu__mobile");
+    const ulwrap = document.querySelector(".ulwrap");
 
-menuBtn.addEventListener("click", menuOpen );
+    menuBtn.addEventListener("click", menuOpen);
 
-closeMenuButton.addEventListener("click", menuClose);
+    closeMenuButton.addEventListener("click", menuClose);
 
-function menuOpen() {
-    menuMobile.classList.add("is-active");
-    ulwrap.classList.add("is-active");
+    function menuOpen() {
+        menuMobile.classList.add("is-active");
+        ulwrap.classList.add("is-active");
+    }
+
+    function menuClose() {
+        menuMobile.classList.remove("is-active");
+        ulwrap.classList.remove("is-active");
+    }
+
+
+    const enterBtn = document.querySelector(".login-link");
+    const formClose = document.querySelector(".modal__form-close");
+    const form = document.querySelector(".modal__form");
+
+
+    enterBtn.addEventListener("click", openForm);
+    formClose.addEventListener("click", closeForm);
+
+
+    function openForm() {
+        form.classList.add("is-active");
+        form.classList.remove("is-closed");
+    }
+
+    function closeForm() {
+        form.classList.remove("is-active");
+        form.classList.add("is-closed");
+    }
 }
-function menuClose() {
-    menuMobile.classList.remove("is-active");
-    ulwrap.classList.remove("is-active");
-}
 
 
-}
+
+
+
+
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", pageLoaded);
 
